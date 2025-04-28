89a978a7589bacf2ef49f53d5d865697fe077b66
Raxan7 <manyerere201@gmail.com>
2025-04-28 10:34:05 +0300

    Feat: Created utils implementation to handle image uploads

---
+++ lib/utils/file_utils.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
+
+class FileUtils {
+  static Future<String> getTemporaryFilePath(Uint8List bytes, String extension) async {
+    if (kIsWeb) {
+      // For web, we'll handle differently
+      throw UnsupportedError('getTemporaryFilePath not supported on web');
+    } else {
+      final tempDir = await getTemporaryDirectory();
+      final file = File('${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.$extension');
+      await file.writeAsBytes(bytes);
+      return file.path;
+    }
+  }
+
+  static Future<String> saveFileForUpload(dynamic fileData) async {
+    if (fileData is File) {
+      return fileData.path;
+    } else if (fileData is Uint8List) {
+      if (kIsWeb) {
+        // For web, we'll upload directly from bytes
+        throw UnsupportedError('Web upload needs special handling');
+      } else {
+        return await getTemporaryFilePath(fileData, 'jpg');
+      }
+    }
+    throw ArgumentError('Unsupported file type');
+  }
+}
